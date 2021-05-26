664bb4ccaeed326258af30ccef37058b1b019820
qiangang <qiangang@hatech.com.cn>
2021-05-26 23:00:01 +0800

    :sparkles: headerNav.vue

--- components/headerNav.vue
+++ components/headerNav.vue
@@ -2,27 +2,21 @@
   <div class="header-box-wrap">
     <div class="header-box">
       <header class="header-nav">
-        <n-link to="/" class="logo">
-          <img src="~assets/image/logo.svg" />
-        </n-link>
-        <n-link to="/" active-class="nav-link-active" class="nav-link">
-          首页
-        </n-link>
-        <n-link to="/pins" class="nav-link"> 沸点 </n-link>
-        <n-link to="/books" class="nav-link"> 小册 </n-link>
-        <n-link to="/event/all" class="nav-link"> 活动 </n-link>
-        <el-input size="small"
-          placeholder="探索掘金"
-        />
-        <el-button>
-          创作者中心
-        </el-button>
-        <el-button>
-          写文章
-        </el-button>
-        <el-button>
-          登录
-        </el-button>
+        <div class="nav-list">
+          <n-link to="/" class="logo">
+            <img src="~assets/image/logo.svg" />
+          </n-link>
+          <n-link to="/" active-class="nav-link-active" class="nav-link">
+            首页
+          </n-link>
+          <n-link to="/pins" class="nav-link"> 沸点 </n-link>
+          <n-link to="/books" class="nav-link"> 小册 </n-link>
+          <n-link to="/event/all" class="nav-link"> 活动 </n-link>
+        </div>
+        <el-input class="search-input" size="small" placeholder="探索掘金" suffix-icon="el-icon-search"/>
+        <el-button size="small" type="primary"> 创作者中心 </el-button>
+        <el-button size="small" type="primary"> 写文章 </el-button>
+        <el-button size="small" type="primary" plain> 登录 </el-button>
       </header>
     </div>
   </div>
@@ -35,21 +29,35 @@
   left: 0;
   top: 0;
   .header-box {
-    margin: 0 auto;
-    width: 100%;
-    max-width: 960px;
+    border-bottom: 1px solid #f1f1f1;
     .header-nav {
+      margin: 0 auto;
+      width: 100%;
       height: 5rem;
+      max-width: 960px;
       display: flex;
       align-items: center;
       justify-content: space-between;
-      .nav-link {
-        color: #71777c;
-        font-size: 16px;
-        text-decoration: none;
-        &.nav-link-active {
-          color: #007fff;
+      .nav-list {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        .logo {
+          margin-right: 2rem;
         }
+        .nav-link {
+          color: #71777c;
+          font-size: 16px;
+          text-decoration: none;
+          padding: 0 1.5rem;
+          &.nav-link-active {
+            color: #007fff;
+          }
+        }
+      }
+      .search-input {
+        width: 168px;
+        margin-right: 10px;
       }
     }
   }
